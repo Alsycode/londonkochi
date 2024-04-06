888e07d8ea5b90fe14312d1f8709751bb39cdd78
Alsycode <120390865+Alsycode@users.noreply.github.com>
2024-04-06 14:47:06 +0530

    dsdds

--- pages/services/mailsService.js
+++ pages/services/mailsService.js
@@ -1,33 +1,30 @@
+import nodemailer from "nodemailer";
 
-var nodemailer = require("nodemailer");
-//-----------------------------------------------------------------------------
 export async function sendMail(subject, toEmail, otpText, name, email) {
-var transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-    user: "shyamalfred@gmail.com",
-    pass: "yjkmdhmxtflkbzfk",
-    },
-    tls: {
-        rejectUnauthorized: false // Disable SSL verification
-    }
-});
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "shyamalfred@gmail.com",
+        pass: "yjkmdhmxtflkbzfk",
+      },
+      tls: {
+        rejectUnauthorized: false, // Disable SSL verification
+      },
+    });
 
+    const mailOptions = {
+      from: "shyamalfred@gmail.com",
+      to: "shyamalfred@gmail.com",
+      subject: subject,
+      text: `Name: ${name}\nEmail: ${email}\n\nMessage: ${otpText}`,
+    };
 
-    var mailOptions = {
-        from: "shyamalfred@gmail.com",
-        to: "shyamalfred@gmail.com",
-        subject: subject,
-        text: `Name: ${name}\nEmail: ${email}\n\nMessage: ${otpText}`,
-      };
-
-
-transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-    throw new Error(error);
-    } else {
-    console.log("Email Sent");
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email Sent:", info.response);
     return true;
-    }
-});
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw new Error("Error sending email");
+  }
 }
