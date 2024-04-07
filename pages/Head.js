2d062d5ebee12d7affc30cb00b0e192fe27acf11
Alsycode <120390865+Alsycode@users.noreply.github.com>
2024-04-07 09:50:56 +0530

    7-4-2024 tab text

--- pages/Head.js
+++ pages/Head.js
@@ -5,7 +5,7 @@ const PageHead = ({ title }) => {
     <>
       <Head>
         <title>{title}</title>
-        <meta name="description" content="Page Description" />
+        <meta name="description" content="We have been succesfull in placing more than 1800+ students  in India and Middle East Asia.London college is a globally reputed  educational institution" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon (1).ico" />
       </Head>
